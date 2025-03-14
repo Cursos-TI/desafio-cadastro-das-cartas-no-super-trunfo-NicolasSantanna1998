e7db8dfb8c76cf865541cea360ae5309a5e282cc
NicolasSantanna1998 <nicolascaputto32@gmail.com>
2025-03-14 21:43:35 +0000

    desafio aventureiro

---
+++ desafioaventureiro.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+    printf("***super trunfo***\n");
+
+    
+    int populacao;
+    char nome [4];
+    float area;
+    float pib;
+    int numerosdepontosturisticos;
+    float densidadepopulacional;
+    float pibpercapita;
+
+    printf("insira os dados carta 1:\n");
+
+
+
+    printf("insira o codigo:\n");
+    scanf("%s" , nome);
+    printf("insira a populaçao:\n");
+    scanf("%d" , &populacao);
+    printf("insira a area:\n");
+    scanf("%f" , &area);
+    printf("insira o pib:\n");
+    scanf("%f" , &pib);
+    printf("insira numero de pontos turisticos:\n");
+    scanf("%d" , &numerosdepontosturisticos);
+    
+
+    printf("insira os dados carta 2:\n");
+
+
+
+    printf("insira o codigo:\n");
+    scanf("%s" , nome);
+    printf("insira a populaçao:\n");
+    scanf("%d" , &populacao);
+    printf("insira a area:\n");
+    scanf("%f" , &area);
+    printf("insira o pib:\n");
+    scanf("%f" , &pib);
+    printf("insira numero de pontos turisticos:\n");
+    scanf("%d" , &numerosdepontosturisticos);
+
+}
