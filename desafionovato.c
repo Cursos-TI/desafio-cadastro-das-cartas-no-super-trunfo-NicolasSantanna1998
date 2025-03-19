052edf11865e3f401971103e37503c15c8927e3a
NicolasSantanna1998 <nicolascaputto32@gmail.com>
2025-03-19 17:14:32 -0300

    Update desafionovato.c

--- desafionovato.c
+++ desafionovato.c
@@ -3,13 +3,23 @@
 int main(){
     printf("***super trunfo***\n");
 
-   
+    
     int populacao;
+    char nome [4];
+    char estado [15];
     float area;
     float pib;
     int numerosdepontosturisticos;
+    
+
+    printf("insira os dados carta 1:\n");
 
 
+
+    printf("insira o codigo:\n");
+    scanf("%s" , nome);
+    printf("insira o nome do estado:\n");
+    scanf("%s" , estado);
     printf("insira a populaçao:\n");
     scanf("%d" , &populacao);
     printf("insira a area:\n");
@@ -19,6 +29,23 @@ int main(){
     printf("insira numero de pontos turisticos:\n");
     scanf("%d" , &numerosdepontosturisticos);
 
-
     
+
+    printf("insira os dados carta 2:\n");
+
+
+
+    printf("insira o codigo:\n");
+    scanf("%s" , nome);
+    printf("insira o nome do estado:\n");
+    scanf("%s" , estado);
+    printf("insira a populaçao:\n");
+    scanf("%d" , &populacao);
+    printf("insira a area:\n");
+    scanf("%f" , &area);
+    printf("insira o pib:\n");
+    scanf("%f" , &pib);
+    printf("insira numero de pontos turisticos:\n");
+    scanf("%d" , &numerosdepontosturisticos);
+
 }
